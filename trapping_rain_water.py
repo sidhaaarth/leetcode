3564fd3736acdd4fe69fd51ca22f63acf9359647
Sidharth <76815920+sidhaaarth@users.noreply.github.com>
2025-05-22 00:24:36 -0400

    added solution for trapping rain water

---
+++ trapping_rain_water.py
@@ -0,0 +1,30 @@
+Trapping Rain Water
+You are given an array non-negative integers height which represent an elevation map. Each value height[i] represents the height of a bar, which has a width of 1.
+
+Return the maximum area of water that can be trapped between the bars.
+
+Input: height = [0,2,0,3,1,0,1,3,2,1]
+
+Output: 9
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        if n == 0:
+            return 0
+        
+        leftMax = [0] * n
+        rightMax = [0] * n
+        
+        leftMax[0] = height[0]
+        for i in range(1, n):
+            leftMax[i] = max(leftMax[i - 1], height[i])
+        
+        rightMax[n - 1] = height[n - 1]
+        for i in range(n - 2, -1, -1):
+            rightMax[i] = max(rightMax[i + 1], height[i])
+        
+        res = 0
+        for i in range(n):
+            res += min(leftMax[i], rightMax[i]) - height[i]
+        return res
